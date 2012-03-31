d72a8414d7e6d6e30b8acc1c4b8dc3dc3e87e2be
Jan Pieter Waagmeester <jieter@jieter.nl>
2012-03-31 12:47:55 +0200

    Finished BioBrickReader and TestBioBrickReader

--- program/WEB-INF/src/synthbio/files/BioBrickReader.java
+++ program/WEB-INF/src/synthbio/files/BioBrickReader.java
@@ -3,12 +3,15 @@
  * https://github.com/FelixAkk/synthbio
  */
 
-package synthbio.models;
+package synthbio.files;
+
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import synthbio.models.*;
 
 public class BioBrickReader{
 
@@ -22,20 +25,50 @@ public class BioBrickReader{
 	/**
 	 * loads BioBrick information from the location specified.
 	 *
-	 * Expects three files in path: and.csv, cds.
+	 * Expects three files in path: and.csv, cds.csv and not.csv
 	 *
 	 * @param 	path
 	 * @throws FileNotFoundException 
 	 */
-	public BioBrickReader(String path){
+	public BioBrickReader(String path) throws Exception{
+
+		String[] rows;
+
+		rows=this.getDataRows(path+"and.csv");
+		this.andPromotors=new AndPromotor[rows.length];
+		for(int i=0; i<rows.length; i++){
+			if(rows[i]!=""){
+				this.andPromotors[i]=AndPromotor.fromCSV(rows[i]);
+			}
+		}
 		
+		rows=this.getDataRows(path+"cds.csv");
+		this.CDSs=new CDS[rows.length];
+		for(int i=0; i<rows.length; i++){
+			if(rows[i]!=""){
+				this.CDSs[i]=CDS.fromCSV(rows[i]);
+			}
+		}
+		
+		rows=this.getDataRows(path+"not.csv");
+		this.notPromotors=new NotPromotor[rows.length];
+		for(int i=0; i<rows.length; i++){
+			if(rows[i]!=""){
+				this.notPromotors[i]=NotPromotor.fromCSV(rows[i]);
+			}
+		}
 		
 		
 	}
 	private String[] getDataRows(String filename) throws Exception{
 		Scanner scan=new Scanner(new File(filename));
-		
-		return null;
+
+		scan.nextLine();
+		ArrayList<String> ret=new ArrayList();
+		while(scan.hasNextLine()){
+			ret.add(scan.nextLine());
+		}
+		return ret.toArray(new String[ret.size()]);
 		
 	}
 
--- program/WEB-INF/src/synthbio/files/test/TestBioBrickReader.java
+++ program/WEB-INF/src/synthbio/files/test/TestBioBrickReader.java
@@ -9,11 +9,29 @@ import org.junit.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import synthbio.files.BioBrickReader;
 
 public class TestBioBrickReader{
 
+	String biobrickPath="biobricks/";
+
+	public static BioBrickReader bbr;
+	/**
+	 * Verify that no exceptions are thrown.
+	 */
 	@Test
-	public void testStub(){
-		
+	public void testLoading() throws Exception{
+		bbr=new BioBrickReader(this.biobrickPath);
+	}
+
+	/**
+	 * Check if reader returns the right amount of biobricks
+	 * per kind.
+	 */
+	@Test
+	public void testBioBrickCounts(){
+		assertEquals(45, bbr.getAndPromotors().length);
+		assertEquals(10, bbr.getCDSs().length);
+		assertEquals(10, bbr.getNotPromotors().length);
 	}
 }
