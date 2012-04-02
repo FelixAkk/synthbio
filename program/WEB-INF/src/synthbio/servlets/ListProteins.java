0085e4c0b19ef201b1d1e835673b726f9e03bbbb
Jan Pieter Waagmeester <jieter@jieter.nl>
2012-04-02 16:24:18 +0200

    Created BioBrickReader + test

---
+++ program/WEB-INF/src/synthbio/servlets/ListProteins.java
@@ -0,0 +1,53 @@
+/**
+ * Synthetic Biology project (Biobrick Modeller/Simulator)
+ * https://github.com/FelixAkk/synthbio
+ */
+
+package synthbio.servlets;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import synthbio.json.JSONResponse;
+import synthbio.files.BioBrickReader;
+import synthbio.models.CDS;
+
+
+/**
+ * Servlet ListServlets returns a default JSON-reply with a list of
+ * proteins available.
+ *
+ * 
+ */
+public class ListProteins extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+
+
+		JSONResponse json=new JSONResponse();
+
+
+		String path=this.getServletContext().getRealPath("/")+"WEB-INF/biobricks/";
+		
+		try{
+			json.data=new BioBrickReader(path).getCDSs();
+			json.success=true;
+		}catch(Exception e){
+			
+			json.success=false;
+			json.message="Could not load BioBricks: "+e.getMessage();
+		}
+
+		out.println(json.toJSONString());
+	}
+}
---
+++ program/WEB-INF/src/synthbio/servlets/test/TestListProteins.java
@@ -0,0 +1,77 @@
+/**
+ * Synthetic Biology project (Biobrick Modeller/Simulator)
+ * https://github.com/FelixAkk/synthbio
+ */
+
+package synthbio.servlets.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.TextPage;
+
+/**
+ * Testing the Servlet ListProteins
+ *
+ */
+public class TestListProteins{
+	/**
+	 * Server location
+	 */
+	 public String url="http://localhost:8080/ListProteins";
+
+
+	
+	/**
+	 * compare response from ListProteins to expected response.
+	 *
+	 * whitespace is not relevant. Could be done better with specialized
+	 * JSON matchers...
+	 */
+	@Test
+	public void testResponse() throws Exception{
+		final WebClient webClient = new WebClient();
+		final TextPage page = webClient.getPage(url);
+
+		String expected=fileToString("src/synthbio/servlets/test/listProteins-expected.json").trim();
+		
+		assertEquals(
+			expected,
+			page.getContent().trim()
+		);
+
+		webClient.closeAllWindows();
+	}
+
+	/**
+	 * quick and dirty file2string method:
+	 * 
+	 * from: http://stackoverflow.com/questions/5471406
+	 */
+	private String fileToString(String filename) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		StringBuilder builder = new StringBuilder();
+		String line;    
+
+		// For every line in the file, append it to the string builder
+		while((line = reader.readLine()) != null){
+			builder.append(line);
+		}
+
+		return builder.toString();
+	}
+   
+
+}
+/*
+0237,"d2":0.7407}]}[]>
+0237,"d2":0.7407}]}[]>
+*/
+
+
