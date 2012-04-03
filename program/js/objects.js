9141638e1d5873a2f4ba092038253f455fb0d96e
Thomas van Helden <Thomas van Helden@ThomasVaio.(none)>
2012-04-04 01:10:08 +0200

    Capitalized classes :D

--- program/js/objects.js
+++ program/js/objects.js
@@ -5,20 +5,20 @@
  */
 
 /**Testable objects*/
-var gate1 = new gate("","x","y");
-var signal1 = new signal("input1","input2","output1","output2");
-var protein1 = new protein("protein 1", "k2", "d1", "d2");
-var protein2 = new protein("protein 2", "k2", "d1", "d2");
+var gate1 = new Gate("","x","y");
+var signal1 = new Signal("input1","input2","output1","output2");
+var protein1 = new Protein("protein 1", "k2", "d1", "d2");
+var protein2 = new Protein("protein 2", "k2", "d1", "d2");
 
 /**Gate Object*/
-function gate(n,x,y) {
+function Gate(n,x,y) {
 	this.name=n;
 	this.Xcoord=x;
 	this.Ycoord=y;
 };
 
 /**Signal Object*/
-function signal(i1, i2, o1, o2) {
+function Signal(i1, i2, o1, o2) {
 	this.input1 = i1;
 	this.input2 = i2;
 	this.output1 = o1;
@@ -26,7 +26,7 @@ function signal(i1, i2, o1, o2) {
 };
 
 /**Protein Object*/
-function protein(n, k, dOne, dTwo) {
+function Srotein(n, k, dOne, dTwo) {
 	this.name = n;
 	this.k2 = k;
 	this.d1 = dOne;
