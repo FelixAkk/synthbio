b74fb0c683c8b9de6e0e420917217810f7e15064
Jan Pieter Waagmeester <jieter@jieter.nl>
2012-04-03 22:04:06 +0200

    Added list of proteins loaded from the server in a bootstrap modal, but cannot get the madals hidden by default.

--- program/js/gui.js
+++ program/js/gui.js
@@ -5,4 +5,39 @@ $(document).ready(function() {
 	/* Activate zhe Dropdowns Herr Doktor! */
 	$('.dropdown-toggle').dropdown();
 	
+	//~ $('.modal').modal('hide');
+
+	/**
+	 * Load proteins from server.
+	 */
+	$('#list-proteins').on('show', function(){
+		$.ajax({
+			url: "/ListProteins",
+			dataType: "json",
+			success: function(response){
+				//on failure, display error message
+				if(!response.success){
+					$('#list-proteins tbody td').html(data.response);
+					return;
+				}
+				
+				//clear the table...
+				$('#list-proteins tbody').html('');
+				
+				//...and insert rows.
+				for(i in response.data){
+					row=response.data[i];
+					rowhtml='<tr>'+
+						'<td>'+row.name+'</td>'+
+						'<td>'+row.k2+'</td>'+
+						'<td>'+row.d1+'</td>'+
+						'<td>'+row.d2+'</td></tr>';
+					
+					$('#list-proteins tbody').append(rowhtml);
+				}
+				
+			}
+		});
+			
+	});
 });
