fe069b07c8fac9f8d4935fc2f609c1232dac5f8c
Thomas van Helden <Thomas van Helden@ThomasVaio.(none)>
2012-04-05 15:01:48 +0200

    reconstructed the whole scaffolding with the help of Jieter. Some work to be done still. Tests for CDS, Gates and Signals about done. The actual update....

--- program/js/objects.js
+++ program/js/objects.js
@@ -6,137 +6,42 @@
  * Definition of objects
  */
 
-/**Testable objects*/
-var and = new AndPromoter("tf1", "tf2", "k1", "km", "n");
-var not = new NotPromoter("tf", "k1", "km", "n");
-var cds1 = new CDS("cds1", "k2", "d1", "d2");
-var cds2 = new CDS("cds2", "k2", "d1", "d2");
-var and2 = jsonToAnd("{\"tf1\":\"tf1\",\"tf2\":\"tf2\",\"k1\":\"k1\",\"km\":\"km\",\"n\":\"n\"}");
-var not2 = jsonToNot("{\"tf\":\"tf\",\"k1\":\"k1\",\"km\":\"km\",\"n\":\"n\"}");
-var cds3 = jsonToCDS("{\"name\":\"cds1\",\"k2\":\"k2\",\"d1\":\"d1\",\"d2\":\"d2\"}");
-
-
-/**AndPromoter
- * ToString() returns a list of all properties of AndPromoter
+/** 
+ * Gates 
+ * Gates hold a type, an X coordinate and a Y coordinate
  */
-function AndPromoter(transFactor1, transFactor2, kOne, kM, hillCoef){
-	this.tf1 = transFactor1;
-	this.tf2 = transFactor2;
-	this.k1 = kOne;
-	this.km = kM;
-	this.n = hillCoef;
+function Gate(t, xCoord, yCoord){
+	this.type = t;
+	this.x = xCoord;
+	this.y = yCoord;
 }
-AndPromoter.prototype.toString = function(){
-	return "AND - tf1 = " + this.tf1 + ", tf2 = "+ this.tf2+ ", k1 = " + this.k1+ ", km = " + this.km+ ", n = " + this.n;
+Gate.prototype.toString = function(){
+	return this.type + ": X = " + this.x + ", Y = "+ this.y;
 }
 
-/**NotPromoter
- * ToString() returns a list of all properties of NotPromoter
+/**
+ * Signals
+ * Signals hold proteins, an origin and a destination
  */
-function NotPromoter(transFactor, kOne, kM, hillCoef){
-	this.tf = transFactor;
-	this.k1 = kOne;
-	this.km = kM;
-	this.n = hillCoef;
+function Signal(prot, origin, destination){
+	this.protein = prot;
+	this.from = origin;
+	this.to = destination;
 }
-NotPromoter.prototype.toString = function(){
-	return "NOT - tf1 = " + this.tf1 + ", k1 = " + this.k1+ ", km = " + this.km+ ", n = " + this.n;
+Signal.prototype.toString = function(){
+	return this.protein + " links " + this.from + " with " + this.to;
 }
 
-/**CDS Object
- * ToString() returns a list of all properties of a CDS
+/**
+ * CDSs
+ * CDSs holds proteinName, k2, d1, d2
  */
-function CDS(n, k, dOne, dTwo) {
-	this.name = n;
+function CDS(proteinName, k, dOne, dTwo) {
+	this.name = proteinName;
 	this.k2 = k;
 	this.d1 = dOne;
 	this.d2 = dTwo;
 }
 CDS.prototype.toString = function(){
 	return "CDS - name = " + this.name + ", k2 = "+ this.k2+ ", d1 = " + this.d1+ ", d2 = " + this.d2;
-}
-
-/**
- * Function which will request an AndPromoter from the server and translate it to a AndPromoter object.
- * Parameter: whichPromoter is used to specify which AndPromoter you wish to get.
- */
-function getAndPromoter(whichPromoter){
-	/**Which data do we want*/
-	$.getJSON("URl",
-	{
-		//whichPromoter
-	},
-	/**What do we want to do with the data*/
-	function(json) {
-		//Make an AndPromoter object out of it
-		return jsonToAnd(json);
-	});
-}
-
-/** Method which takes JSON string and converts it into an AndPromoter*/
-function jsonToAnd(json){
-	var temp = $.parseJSON(json);
-	return new AndPromoter(temp.tf1, temp.tf2, temp.k1, temp.km, temp.n);
-}
-
-/**
- * Function which will request an NotPromoter from the server and translate it to a NotPromoter object.
- * Parameter: whichPromoter is used to specify which NotPromoter you wish to get.
- */
-function getNotPromoter(whichPromoter){
-	/**Which data do we want*/
-	$.getJSON("URl",
-	{
-		//whichPromoter
-	},
-	/**What do we want to do with the data*/
-	function(json) {
-		//Make an NotPromoter object out of it
-		return jsonToNot(json);
-	});
-}
-
-/** Method which takes JSON string and converts it into an NotPromoter*/
-function jsonToNot(json){
-	var temp = $.parseJSON(json);
-	return new NotPromoter(temp.tf, temp.k1, temp.km, temp.n);
-}
-
-/**
- * Function which will request CDS from the server and translate it to a CDS object.
- * Parameter: whichPromoter is used to specify which CDS you wish to get.
- */
-function getCDS(whichCDS){
-	/**Which data do we want*/
-	$.getJSON("URl",
-	{
-		//whichCDS
-	},
-	/**What do we want to do with the data*/
-	function(json) {
-		//Make an CDS object out of it
-		return jsonToCDS(json);
-	});
-}
-
-/** Method which takes JSON string and converts it into CDS*/
-function jsonToCDS(json){
-	var temp = $.parseJSON(json);
-	return new CDS(temp.name, temp.k2, temp.d1, temp.d2);
-}
-
-/**
- * Function which will request a calculation from the server.
- * Parameter: the circuit which we want to calculate the output of.
- */
-function calcRes(circuit){
-	/**Which data do we want*/
-	$.getJSON("URl",
-	{
-		//circuit
-	},
-	/**What do we want to do with the data*/
-	function(json) {
-		//Show result somehow
-	});
 }
--- program/js/tests.js
+++ program/js/tests.js
@@ -6,45 +6,122 @@
  * This document contains tests for the JavaScript clientside
  * The JSON coming from the Java server will be translated to circuits and UnitTested here
  */
+ 
+/**
+ * Testable objects
+ */
+var gate = new Gate("and", 10,20);
+var signal = new Signal(A,1,2);
+var cds1 = new CDS("cds1", "k2", "d1", "d2");
+var cds2 = new CDS("cds2", "k2", "d1", "d2");
+
+/**
+ * The actual tests
+ */
  $(document).ready(function(){
 
-	/**AND Promoters*/ 
-	module("ANDPromoters");
-
-		test("AndPromoters should contain", function() {
-		  equal( and.tf1 , "tf1", "TranscriptionFactor1" );
-		  equal( and.tf2 , "tf2", "TranscriptionFactor2" );
-		  equal( and.k1 , "k1", "K1" );
-		  equal( and.km , "km", "Km" );
-		  equal( and.n , "n", "the Hill Coeffectient" );
+	/**
+	 * Gates
+	 */
+	module("Gates");
+	
+		/**
+		 * Gate properties
+		 */
+		test("Gates should have the right properties", function(){
+			equal(gate.type, "type", "Gates store types");
+			equal(gate.x, "x", "Gates store X coordinates");
+			equal(gate.y, "y", "Gates store Y coordinates");
 		});
 		
-		test("toString method test of AndPromoter", function(){
-			deepEqual(and.toString(), "AND - tf1 = " + and.tf1 + ", tf2 = "+ and.tf2+ ", k1 = " + and.k1+ ", km = " + and.km+ ", n = " + and.n, "The list of properties is returned");
-		});
- 
-	/**NOT Promoters*/ 
-	module("NotPromoters");
-
-		test("NotPromoters should contain", function() {
-		  equal( not.tf , "tf", "TranscriptionFactor" );
-		  equal( not.k1 , "k1", "K1" );
-		  equal( not.km , "km", "Km" );
-		  equal( not.n , "n", "the Hill Coeffectient" );
+		/**
+		 * Gate toString
+		 */
+		test("Gates should have a working toString method", function(){
+			equal(gate.toString(), gate.type + ": X = " + gate.x + ", Y = "+ gate.y ,"The toString method works");
 		});
 		
-		test("toString method test of NotPromoter", function(){
-			deepEqual(not.toString(),"NOT - tf1 = " + not.tf1 + ", k1 = " + not.k1+ ", km = " + not.km+ ", n = " + not.n, "The list of properties is returned");
+		/**
+		 * From Gate to JSON
+		 */
+		test("Gates should be able to be constructed from JSON", function(){
+			equal(gate.toJSON(), "{\"type\":\"and\",\"x\":\"10\",\"y\":\"20\"}", "Gates can be converted to JSON");
 		});
 		
-	/**What data should be stored in CDS?*/
+		/**
+		 * From JSON to Gate
+		 */
+		test("Gates should be able to be constructed from JSON", function(){
+			deepEqual(Gate.fromJSON("{\"type\":\"and\",\"x\":\"10\",\"y\":\"20\"}"), gate, "JSON can be converted to Gates");
+		});
+		
+		/**
+		 * Get Gate from server
+		 */
+		test("Request Gate from server and converting it to Gate object", function(){
+			ok(false, "needs testing");
+		});
+		
+		
+	/**
+	 *Signals
+	 */
+	module("Signals");
+		
+		/**
+		 * Signal properties
+		 */
+		test("Signals should store the right properties", function(){
+			equal(signal.protein, "A", "type of protein");
+			equal(signal.from, "1", "Origin of signal");
+			equal(signal.to, "2", "Destination of signal");
+		});
+		
+		/**
+		 * Signal toString
+		 */
+		test("Signals should have a toString method", function(){
+			equal(signal.toString(), signal.protein + " links " + signal.from + " with " + signal.to);
+		});
+		
+		/**
+		 * From Signal to JSON
+		 */
+		test("Signal should be able to be constructed from JSON", function(){
+			equal(signal.toJSON(), "{\"protein\":\"A\",\"from\":\"1\",\"to\":\"2\"}", "Signals can be translated to JSON");
+		});
+		
+		/**
+		 * From JSON to Signal
+		 */
+		test("Signal should be able to be constructed from JSON", function(){
+			deepEqual(Signal.fromJSON("{\"protein\":\"A\",\"from\":\"1\",\"to\":\"2\"}"), signal, "JSON can be converted to Signals");
+		});
+		
+		/**
+		 * Get Signal from server
+		 */
+		test("Request Signal from server and converting it to Signal object", function(){
+			ok(false, "needs testing");
+		});
+		
+		
+	/**
+	 * CDS
+	 */
 	module("CDS");
 
+		/**
+		 * CDS list
+		 */
 		test("All CDSs should be listed", function() {
 		  equal( cds1.name , "cds1", "cds1" );
 		  equal( cds2.name , "cds2", "cds2" );
 		});
 
+		/**
+		 * CDS properties
+		 */
 		test("First part of CDS has values", function() {
 		  equal( cds1.name , "cds1", "cds1" );
 		  equal( cds1.k2 , "k2", "K2" );
@@ -52,98 +129,33 @@
 		  equal( cds1.d2 , "d2", "D2" );
 		});
 		
+		/**
+		 * CDS toString
+		 */
 		test("toString method test of CDS", function(){
 			deepEqual(cds1.toString(), "CDS - name = " + cds1.name + ", k2 = "+ cds1.k2+ ", d1 = " + cds1.d1+ ", d2 = " + cds1.d2, "The list of properties is returned");
 		});
 		
-	/**What is the connection info?*/
-	module("Connection info & data transfer");
-	
-		test("connection has a name", function(){
-			ok(true, "needs testing");
-		});
-		
-		test("See Ping Pong test", function(){
-			ok(true, "needs testing");
-		});
-		
-	/**Does the circuit export work?*/
-	module("Export");
-	
-		test("A AndPromoter can be converted to JSON", function(){
-			deepEqual(JSON.stringify(and), "{\"tf1\":\"tf1\",\"tf2\":\"tf2\",\"k1\":\"k1\",\"km\":\"km\",\"n\":\"n\"}","converting AND to JSON");
-		});
-		
-		test("A NotPromoter can be converted to JSON", function(){
-			deepEqual(JSON.stringify(not), "{\"tf\":\"tf\",\"k1\":\"k1\",\"km\":\"km\",\"n\":\"n\"}","converting AND to JSON");
-		});
-		
+		/**
+		 * From CDS to JSON
+		 */
 		test("CDS can be converted to JSON", function(){
-			deepEqual(JSON.stringify(cds1), "{\"name\":\"cds1\",\"k2\":\"k2\",\"d1\":\"d1\",\"d2\":\"d2\"}","converting AND to JSON");
+			deepEqual(cds1.toJSON, "{\"name\":\"cds1\",\"k2\":\"k2\",\"d1\":\"d1\",\"d2\":\"d2\"}","converting CDS to JSON");
 		});
 		
-		test("An object can be sent to the server as JSON", function(){
-			ok(true, "needs testing");
-		});
-		
-		
-	/**Can we import data via JSON from Java?*/
-	module("Import");
-		
-		test("Be able to receive JSON from Java server", function(){
-			ok(true, "needs testing");
-		});
-		
-		/**Using jsonToAnd method to convert JSON to Javascript Object*/
-		test("Needs to be able to parse JSON and convert to AndPromoter, testing jsonToAnd", function(){
-			deepEqual(and2, and, "parsing JSON object of AND");
-		});
-		
-		test("testing getAndPromoter function", function(){
-			ok(true, "needs testing");
-		});
-		
-		/**Using jsonToNot method to convert JSON to Javascript Object*/
-		test("Needs to be able to parse JSON and convert to NotPromoter", function(){
-			deepEqual(not2, not, "parsing JSON object of NOT");
-		});
-		
-		test("testing getNotPromoter function", function(){
-			ok(true, "needs testing");
-		});
-		
-		/**Using jsonToCDS method to convert JSON to Javascript Object*/
+		/**
+		 * From JSON to CDS
+		 */
 		test("Needs to be able to parse JSON and convert to CDS", function(){
-			deepEqual(cds3, cds1, "parsing JSON object of CDS");
+			deepEqual(CDS.fromJSON("{\"name\":\"cds1\",\"k2\":\"k2\",\"d1\":\"d1\",\"d2\":\"d2\"}"), cds1, "parsing JSON object of CDS");
 		});
 		
+		/**
+		 * Get CDS from server
+		 */
 		test("testing getCDS function", function(){
-			ok(true, "needs testing");
+			ok(false, "needs testing");
 		});
 		
-	/**Calculating output*/
-	module("Calculate output");
-		
-		test("setInput", function(){
-			ok(true,"needs testing");
-		});
-		
-		test("setOutput", function(){
-			ok(true,"needs testing");
-		});
-		
-		test("getOutput", function(){
-			ok(true,"needs testing");
-		});
-		
-	
-	/**Validating proteins*/
-	module("Validate proteins");
-		
-		
-	/**Validating circuits*/
-	module("Validate circuit");
-		
-		
-});
  
+}
