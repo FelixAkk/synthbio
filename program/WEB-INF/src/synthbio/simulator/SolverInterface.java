4239a972430dc067e53975ef3b7bd6747c1ce4d9
atennapel <aptennap@gmail.com>
2012-04-26 12:14:15 +0200

    Updated SolverInterface.

--- program/WEB-INF/src/synthbio/simulator/SolverInterface.java
+++ program/WEB-INF/src/synthbio/simulator/SolverInterface.java
@@ -14,32 +14,37 @@
   */
 public class SolverInterface {
 	private final String SBMLSIMULATOR_PATH = "lib/SBMLsimulator.jar";
-	private String result;
 	
-	public SolverInterface solve(String file) {
+	/**
+	 * Takes a sbml file, a time series file (csv), a sbml file (for output of optimized sbml) and a result file (csv) and solves it.
+	 * Returns commandline output
+	 */
+	public String solve(String sbmlIn, String timeIn, String sbmlOut, String results) {
+		String res = "ERROR";
 		try {
 			Runtime rt = Runtime.getRuntime();
-			Process pr = rt.exec("java -jar \""+SBMLSIMULATOR_PATH+"\" --sbml-input-file \""+file+"\"");
+			Process pr = rt.exec(
+				"java -jar \""+SBMLSIMULATOR_PATH+
+				"\" --sbml-input-file \""+sbmlIn+
+				"\" --time-series-file \""+timeIn+
+				"\" --sbml-output-file \""+sbmlOut+
+				"\" --simulation-output-file \""+results+"\""
+			);
+			
 			
 			BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 			
-			String res = "";
+			res = "";
 			String line = null;
 			while((line = input.readLine()) != null) {
 				res += line + "\n";
 			}
-
+			
 			int exitVal = pr.waitFor();
-		
-			result = res;
 		} catch(Exception e) {
 			System.out.println(e.toString());
 			e.printStackTrace();
 		}
-		return this;
-	}
-	
-	public String getStringResult() {
-		return result;
+		return res;
 	}
 } 
