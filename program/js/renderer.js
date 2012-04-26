9dd38a9232722905b37b4d538cb4737a1f1e1acd
niels <niels.a.d@gmail.com>
2012-04-26 12:27:56 +0200

    Source/Target points + demo.

--- program/js/renderer.js
+++ program/js/renderer.js
@@ -17,20 +17,74 @@ $(document).ready(function() {
 		jsPlumb.Defaults.Container = $("#grid-container");
 		jsPlumb.draggable($("#grid-container .gate"));
 
-		var fillColor = "gray"; 
-		jsPlumb.Defaults.Connector = [ "Bezier", { curviness:50 } ];
-		jsPlumb.Defaults.DragOptions = { cursor: "pointer", zIndex:2000 };
-		jsPlumb.Defaults.PaintStyle = { strokeStyle:"gray", lineWidth:2 };
-		jsPlumb.Defaults.EndpointStyle = { radius:9, fillStyle:"gray" };
-		jsPlumb.Defaults.Anchors =  [ "BottomCenter", "TopCenter" ];
+		jsPlumb.importDefaults({
+			// default drag options
+			DragOptions : { cursor: 'pointer', zIndex:2000 },
+			// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+			// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+			ConnectionOverlays : [
+				[ "Arrow", { location:0.9 } ],
+				[ "Label", { 
+					location:0.5,
+					id:"label"
+				}]
+			]
+		});			
 
-		// declare some common values:
-		var arrowCommon = { foldback:0.7, fillStyle:fillColor, width:14 };
-		// use three-arg spec to create two different arrows with the common values:
-		var overlays = [
-			[ "Arrow", { location:0.5 }, arrowCommon ]
-		];
+		// this is the paint style for the connecting lines..
+		var connectorPaintStyle = {
+			lineWidth:3,
+			strokeStyle:"#deea18",
+			joinstyle:"round"
+		},
+		// .. and this is the hover style. 
+		connectorHoverStyle = {
+			lineWidth:5,
+			strokeStyle:"#2e2aF8"
+		},
+		// the definition of source endpoints (the small blue ones)
+		sourceEndpoint = {
+			endpoint:"Dot",
+			paintStyle:{ fillStyle:"#225588",radius:7 },
+			isSource:true,
+			connector:[ "Flowchart", { stub:40 } ],
+			connectorStyle:connectorPaintStyle,
+			hoverPaintStyle:connectorHoverStyle,
+			connectorHoverStyle:connectorHoverStyle,
+			dragOptions:{}
+		},
+		// the definition of target endpoints (will appear when the user drags a connection) 
+		targetEndpoint = {
+			endpoint:"Rectangle",					
+			paintStyle:{ fillStyle:"#558822",width:10,height:10 },
+			hoverPaintStyle:connectorHoverStyle,
+			maxConnections:-1,
+			dropOptions:{ hoverClass:"hover", activeClass:"active" },
+			isTarget:true
+		};			
 
-		jsPlumb.connect({source:"hoi1", target:"hoi2", overlays:overlays});
+		var allSourceEndpoints = [], allTargetEndpoints = [];
+		_addEndpoints = function(toId, sourceAnchors, targetAnchors) {
+			for (var i = 0; i < sourceAnchors.length; i++) {
+				var sourceUUID = toId + sourceAnchors[i];
+				allSourceEndpoints.push(jsPlumb.addEndpoint(toId, sourceEndpoint, { anchor:sourceAnchors[i], uuid:sourceUUID }));
+			}
+			for (var j = 0; j < targetAnchors.length; j++) {
+				var targetUUID = toId + targetAnchors[j];
+				allTargetEndpoints.push(jsPlumb.addEndpoint(toId, targetEndpoint, { anchor:targetAnchors[j], uuid:targetUUID }));
+			}
+		};
+
+		// listen for new connections; initialise them the same way we initialise the connections at startup.
+		jsPlumb.bind("jsPlumbConnection", function(connInfo, originalEvent) { 
+			connInfo.connection.getOverlay("label").setLabel("I am a banana");
+		});
+
+		_addEndpoints("hoi1", ["RightMiddle"], ["TopLeft", "BottomLeft"]);
+		_addEndpoints("hoi2", ["TopRight", "BottomRight"], ["LeftMiddle"]);
+
+		// connect a few up
+		jsPlumb.connect({uuids:["hoi2TopRight", "hoi1TopLeft"]});
+		jsPlumb.connect({uuids:["hoi2BottomRight", "hoi1BottomLeft"]});
 	});
 });
