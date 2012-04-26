67f8d3e95d02e8d3029942d3aa06227f610226cb
Jan Pieter Waagmeester <jieter@jieter.nl>
2012-04-26 12:09:22 +0200

    Created superclass for servlets in the synthbio-context, to organize shared functionality. Created stub for the Circuit(save|load|validate) servlet.

---
+++ program/WEB-INF/src/synthbio/servlets/SynthbioServlet.java
@@ -0,0 +1,39 @@
+/**
+ * Synthetic Biology project (Biobrick Modeller/Simulator)
+ * https://github.com/FelixAkk/synthbio
+ */
+
+package synthbio.servlets;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import synthbio.json.JSONResponse;
+import synthbio.files.BioBrickReader;
+import synthbio.models.CDS;
+
+
+/**
+ * Synthbio abstract servlet containing shared functionality.
+ *
+ * @author jieter 
+ */
+public abstract class SynthbioServlet extends HttpServlet {
+
+
+	/**
+	 * Return the BioBrick repository.
+	 *
+	 * @return A BioBrickReader object.
+	 */
+	public BioBrickReader getBioBrickReaderRepository() throws Exception{
+		String path=this.getServletContext().getRealPath("/")+"WEB-INF/biobricks/";
+		return new BioBrickReader(path);
+	}
+}
