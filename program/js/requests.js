e1fc750c3b51a8d54ba2e81137f7b37e96d364fd
Thomas van Helden <tvanhelden@gmail.com>
2012-05-02 00:31:26 +0200

    made a more complete view of the requests and the tests are done. First gonna build servlets and than completing the requests.js

--- program/js/requests.js
+++ program/js/requests.js
@@ -3,15 +3,12 @@
  * Synthetic biology modeller/simulator
  * https://github.com/FelixAkk/synthbio
  * by Group E, TU Delft
- * @author	jieter
+ * @author	Thomas van Helden, jieter
  *
  * Definition of requests. All requests made from the client to the server
  * should be defined in this file.
  */
  
-/*jslint devel: true, browser: true, sloppy: true, stupid: false, white: true, maxerr: 50, indent: 4 */
-/*global $ */
-
 /**
  * syntbio package.
  */
@@ -46,12 +43,78 @@ synthbio.requests.baseXHR = function(provided){
 }
 
 /**
+ * Define the listFiles method.
+ */
+synthbio.requests.listFiles = function(){
+	
+	function callback {};
+	
+	synthbio.requests.baseXHR({
+		url: "/ListFiles",
+		success: function(response){
+			if(!response.success){
+				return callback(response.message);
+			}
+			return callback(response.data);
+		},
+		error: function(){
+			return callback("Error has occured. Cannot get list of files from server")
+		},
+		always: callback("listFiles called")
+		
+	});
+	
+};
+
+/**
+ * Define the getFile method.
+ */
+synthbio.requests.getFile = function(fileName){
+
+	function callback {};
+	
+	synthbio.requests.baseXHR({
+		url: "/FolderWhereStuffIsSaved/"+ fileName,
+		success: function(response){
+			return callback(response);
+		},
+		error: callback("Error has occured. Cannot get file from server"),
+		always: callback("getFile called")
+		
+	});
+};
+
+/**
+ * Define the putFile method.
+ */
+synthbio.requests.putFile = function(fileName, circ){
+	
+	function callback {};
+	
+	synthbio.requests.baseXHR({
+		url: "/SaveFiles",
+		type: "POST",
+		data: [name: fileName, circuit:JSON.stringify(circ)],
+		success: function(response){
+			if(!response.success){
+				return callback(response.message);
+			}
+			return callback("Storage of "+fileName+" successful");
+		},
+		error: callback("Error has occured. Cannot save file on server"),
+		always: callback("putFile called")
+		
+	});
+};
+
+/**
  * Define the getCDSs request.
  */
 synthbio.requests.getCDSs = function(){
 	//if(!(callback instanceof Function)){
 	//	throw "Callback function should be supplied";
 	//}
+	function callback {};
 	
 	synthbio.requests.baseXHR({
 		url: "/ListProteinsServlet",
@@ -64,33 +127,75 @@ synthbio.requests.getCDSs = function(){
 			$.each(response.data, function(i, elem){
 				list[i]=synthbio.CDS.fromMap(response.data[i]);
 			});
-			return list;
+			return callback(list);
 		},
+		error: callback("Error has occured. Cannot get proteins from server"),
+		always: callback("getCDS called")
 		
 	});
 };
 
 /**
- * Define the listFiles method.
+ * Define the listCircuits method.
  */
-synthbio.requests.listFiles = function(callback){
-	if(!(callback instanceof Function)){
-		throw "Callback function should be supplied";
-	}
+synthbio.requests.listCircuits = function(){
+	
+	function callback {};
 	
 	synthbio.requests.baseXHR({
-		url: "/ListFiles",
+		url: "/ListCircuits",
 		//parse list of cdses and call the callback with that list.
 		success: function(response){
 			if(!response.success){
-				return callback(response.message);
+				return response.message;
 			}
+			var list=[];
+			$.each(response.data, function(i, elem){
+				list[i]=synthbio.Circuit.fromMap(response.data[i]);
+			});
 			return callback(list);
 		},
-		error: function(){
-			return callback("Error has occured. Cannot get list of files from server")
+		error: callback("Error has occured. Cannot get circuits from server"),
+		always: callback("listCircuit called")
+		
+	});
+};
+
+
+/**
+ * Define the simulate method.
+ */
+synthbio.requests.simulate = function(fileName, input){
+	
+	function callback {};
+	
+	synthbio.requests.baseXHR({
+		url: "/simulate",
+		data: [fileName, JSON.stringify(input)]
+		success: function(response){
+			return callback(response);
 		},
-		always: callback()
+		error: callback("Error has occured. Cannot simulate this circuit"),
+		always: callback("simulate called")
+		
+	});
+};
+
+/**
+ * Define the validate method.
+ */
+synthbio.requests.validate = function(fileName, input){
+	
+	function callback {};
+	
+	synthbio.requests.baseXHR({
+		url: "/validate",
+		data: [fileName, JSON.stringify(input)]
+		success: function(response){
+			return callback(response);
+		},
+		error: callback("Error has occured. Cannot validate this circuit. Please check input"),
+		always: callback("simulate called")
 		
 	});
 };
