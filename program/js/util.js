f24da175dc3570fb290ccb7875b3f045c5deec6a
Felix Akkermans <felix.akkermans@gmail.com>
2012-05-03 04:13:24 +0200

    Partly implemented the drag-drop new gates functionality.

---
+++ program/js/util.js
@@ -0,0 +1,49 @@
+/**
+ * Project Zelula
+ *
+ * Contextproject TI2800
+ * TU Delft - University of Technology
+ *
+ * Authors:
+ * 	Felix Akkermans, Niels Doekemeijer, Thomas van Helden
+ * 	Albert ten Napel, Jan Pieter Waagmeester
+ *
+ * https://github.com/FelixAkk/synthbio
+ *
+ * @author Felix Akkermans & Jan-Pieter Waagmeester
+ *
+ * Utility functions for global useage.
+ */
+
+/**
+ * syntbio package.
+ */
+var synthbio = synthbio || {};
+
+/**
+ * requests package
+ */
+synthbio.util = synthbio.util || {};
+
+/**
+ *
+ * @param message
+ */
+synthbio.util.AssertException = function(message) {
+    this.message = message;
+}
+
+synthbio.util.AssertException.prototype.toString = function () {
+    return 'AssertException: ' + this.message;
+};
+
+/**
+ *
+ * @param exp
+ * @param message
+ */
+synthbio.util.assert = function(exp, message) {
+    if (!exp) {
+        throw new AssertException(message);
+    }
+}
