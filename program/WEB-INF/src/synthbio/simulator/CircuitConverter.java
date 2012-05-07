0b368b9f1f1f2928faeed14ff9e3ce2bdb45da54
atennapel <aptennap@gmail.com>
2012-05-07 11:50:37 +0200

    Added the CircuitConverter files.

---
+++ program/WEB-INF/src/synthbio/simulator/CircuitConverter.java
@@ -0,0 +1,33 @@
+/**
+ * Project Zelula
+ *
+ * Contextproject TI2800 
+ * TU Delft - University of Technology
+ *
+ * Authors: 
+ * 	Felix Akkermans, Niels Doekemeijer, Thomas van Helden
+ * 	Albert ten Napel, Jan Pieter Waagmeester
+ *
+ * https://github.com/FelixAkk/synthbio
+ */
+ 
+package synthbio.simulator;
+
+import synthbio.models.Circuit;
+
+/**
+ * A class for converting Circuit-objects to SBML.
+ * @author Albert ten Napel
+ */
+public class CircuitConverter {
+
+	/**
+	 * Converts a Circuit-object to SBML.
+	 * @param		circuit	The Circuit-object to convert.
+	 * @return					The SBML-formatted string.
+	 */
+	public String convert(Circuit circuit) {
+		circuit.validate();
+		return null;
+	}
+} 
