c394fffd729efdc73b71617d95f515d19cf30b15
Jan Pieter Waagmeester <jieter@jieter.nl>
2012-05-10 16:09:23 +0200

    Fix whitespace and namespace issues in util.js

--- program/js/util.js
+++ program/js/util.js
@@ -15,6 +15,9 @@
  * Utility functions for global useage.
  */
 
+/*jslint devel: true, browser: true, sloppy: true, white: true, maxerr: 50, indent: 4 */
+/*global $, synthbio */
+
 /**
  * syntbio package.
  */
@@ -31,22 +34,25 @@ synthbio.util = synthbio.util || {};
  * @param message Debugging message.
  */
 synthbio.util.AssertException = function(message) {
-    this.message = message;
+	this.message = message;
 }
 
 synthbio.util.AssertException.prototype.toString = function () {
-    return 'AssertException: ' + this.message;
+	return 'AssertException: ' + this.message;
 };
 
 /**
- * Ensure that an expression evaluates to true during execution. if the expression evaluates to false, this indicates a
- * possible bug in code.
+ * Ensure that an expression evaluates to true during execution.
+ * If the expression evaluates to false, this indicates a possible bug
+ * in code.
  *
- * @param exp Expression yielding a boolean (or truthy, which is not recommended) value.
+ * @param exp Expression yielding a boolean
+ * (or truthy, which is not recommended) value.
+ *
  * @param message Debugging message.
  */
 synthbio.util.assert = function(exp, message) {
-    if (!exp) {
-        throw new AssertException(message);
-    }
+	if (!exp) {
+		throw new synthbio.util.AssertException(message);
+	}
 }
