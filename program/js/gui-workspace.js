f3bdbbd48470d257e8cf40e42f2f906e685d1d6a
niels <niels.a.d@gmail.com>
2012-06-01 10:57:19 +0200

    Endpoints above wires for easier wire dragging.

--- program/js/gui-workspace.js
+++ program/js/gui-workspace.js
@@ -461,7 +461,7 @@ jsPlumb.ready(function() {
 		//Overlays for wires
 		ConnectionOverlays : [
 			//Arrow overlay
-			[ "Arrow", { location: 1 } ],
+			[ "Arrow", { location: 0.92 } ],
 
 			//Text overlay
 			[ "Label", {
